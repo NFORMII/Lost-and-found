ed843028673e4c4dc9eff59977f996b4a173e004
NFORMII <n.girbong@gmail.com>
2026-02-02 20:31:19 +0200

    storage

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageService {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<String> uploadImage(File file) async {
+    final String fileName =
+        DateTime.now().millisecondsSinceEpoch.toString();
+
+    final ref = _storage.ref().child('items/$fileName.jpg');
+
+    await ref.putFile(file);
+    return await ref.getDownloadURL();
+  }
+}
